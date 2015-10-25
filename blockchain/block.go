de2115ce8abadb825da0196b83e67fda936267a7
Conner Fromknecht <conner@mit.edu>
2015-10-25 16:15:37 -0400

    Double hash working

--- blockchain/block.go
+++ blockchain/block.go
@@ -2,39 +2,38 @@ package blockchain
 
 import (
 	"crypto/sha256"
-	"encoding/base64"
+	"encoding/hex"
 )
 
-type SHA256Sum []byte
+type SHA256Sum [32]byte
 
 type Block struct {
 	PrevHash SHA256Sum
 }
 
 func CertHash(b []byte) SHA256Sum {
-	h256 := sha256.New()
-	h := h256.Sum(b)
-	return h256.Sum(h)
+	hash1 := sha256.Sum256(b)
+	return sha256.Sum256(hash1[:])
 }
 
-func CertHash64(b []byte) SHA256Sum {
+func CertHash64(b []byte) string {
 	h := CertHash(b)
-	return b64Encode(h)
+	return b64Encode(h[:])
 }
 
 func CertHashStr(s string) SHA256Sum {
 	return CertHash([]byte(s))
 }
 
-func CertHash64Str(s string) SHA256Sum {
+func CertHash64Str(s string) string {
 	h := CertHashStr(s)
-	return b64Encode(h)
+	return b64Encode(h[:])
 }
 
 func b64Encode(b []byte) string {
-	return base64.StdEncoding.EncodeToString(b)
+	return hex.EncodeToString(b)
 }
 
 func b64Decode(s string) ([]byte, error) {
-	return base64.StdEncoding.DecodeStr(s)
+	return hex.DecodeString(s)
 }
--- run/main.go
+++ run/main.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	fmt.Println("Hello world")
 	fmt.Println("Hashing hello")
-	fmt.Println(blockchain.CertHashStr("hello"))
+	fmt.Println(blockchain.CertHash64Str("hello"))
 }
